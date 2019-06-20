8b07835e7694d1097bf28d821434421a67f3dff6
Rommel Rico <me@rommelrico.com>
2019-06-20 07:09:49 -0700

    feat(App): Using network images

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { StyleSheet, View } from 'react-native';
 import PlaceList from "./src/components/placelist/PlaceList";
 import PlaceInput from "./src/components/placeinput/PlaceInput";
-import placeImage from "./src/assets/SanDiego.jpg";
+// import placeImage from "./src/assets/SanDiego.jpg";
 
 export default class App extends Component {
 
@@ -16,7 +16,9 @@ export default class App extends Component {
         {
           key: Math.random().toString(),
           name: placeName,
-          image: placeImage
+          image: {
+            uri: "https://www.gannett-cdn.com/-mm-/fa888cd8ba5934840efa2cd0be4e477c78b2b1d1/c=0-42-2118-1239&r=x1683&c=3200x1680/local/-/media/2016/07/25/Phoenix/Phoenix/636050500824809040-ThinkstockPhotos-516182342.jpg"
+          }
         }
       )};
     });
