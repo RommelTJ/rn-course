c46ab67bca0a8baab130e71a52957d05cdcaec5a
Rommel Rico <me@rommelrico.com>
2019-06-22 08:14:56 -0700

    feat(actions): Delete Place action creator

--- src/store/actions/places.js
+++ src/store/actions/places.js
@@ -1,8 +1,14 @@
-import {ADD_PLACE} from "./actionTypes";
+import {ADD_PLACE, DELETE_PLACE} from "./actionTypes";
 
 export const addPlace = (placeName) => {
   return {
     type: ADD_PLACE,
     payload: placeName
+  };
+};
+
+export const deletePlace = () => {
+  return {
+    type: DELETE_PLACE
   };
 };
