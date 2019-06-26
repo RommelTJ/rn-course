24cb2900955c0d85515e46b486a7f169fdd08042
Rommel Rico <me@rommelrico.com>
2019-06-26 07:19:29 -0700

    testing: Using an icon

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ import PlaceInput from "./src/components/placeinput/PlaceInput";
 import PlaceDetail from "./src/components/placedetail/PlaceDetail";
 import { addPlace, deletePlace, selectPlace, deselectPlace } from './src/store/actions/index';
 import configureStore from "./src/store/configureStore";
+import { Ionicons } from '@expo/vector-icons';
 
 const mapStateToProps = state => {
   return {
@@ -53,6 +54,7 @@ class App extends Component {
                      onModalClosed={this.modalClosedHandler}
         />
         <PlaceInput onPlaceAdded={this.placeAddedHandler} />
+        <Ionicons name="md-checkmark-circle" size={32} color="green" />
         <PlaceList places={this.props.places} onItemSelected={this.placeSelectedHandler} />
       </View>
     );
