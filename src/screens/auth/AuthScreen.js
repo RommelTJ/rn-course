33a0666b03953bf41cf01911805e9f4544cef14e
Rommel Rico <me@rommelrico.com>
2019-06-29 11:23:35 -0700

    feat(AuthScreen): Adding a title

--- src/screens/auth/AuthScreen.js
+++ src/screens/auth/AuthScreen.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 
 class AuthScreen extends Component {
+
+  static navigationOptions = {
+    title: 'Please sign in',
+  };
+
   render() {
     return (
       <View>
