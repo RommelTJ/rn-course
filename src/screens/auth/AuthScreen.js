59f1c31ef067bc5a122bb3cdd4839670e96de7e4
Rommel Rico <me@rommelrico.com>
2019-07-09 07:18:18 -0700

    feat(AuthScreen): Centering using Flexbox

--- src/screens/auth/AuthScreen.js
+++ src/screens/auth/AuthScreen.js
@@ -28,7 +28,9 @@ class AuthScreen extends Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
   }
 });
 
