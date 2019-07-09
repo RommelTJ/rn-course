abb34770a03fe3ead44448f325ec897be2260abb
Rommel Rico <me@rommelrico.com>
2019-07-08 21:41:38 -0700

    feat(styling): Common styles

---
+++ src/styles/index.js
@@ -0,0 +1,11 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  headerButton: {
+    height: 44,
+    width: 44,
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  }
+});
