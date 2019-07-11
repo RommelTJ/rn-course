8f393c53a10808c6fa1292a588d0475a8379ad16
Rommel Rico <me@rommelrico.com>
2019-07-11 07:13:53 -0700

    feat(AuthScreen): Adding some styling to the login box

--- src/screens/auth/AuthScreen.js
+++ src/screens/auth/AuthScreen.js
@@ -18,9 +18,9 @@ class AuthScreen extends Component {
         <Text>Please Log In</Text>
         <View style={styles.inputContainer}>
           <Button title="Switch to Login" />
-          <TextInput placeholder="Your Email Address" style={styles.input} />
-          <TextInput placeholder="Password" style={styles.input} />
-          <TextInput placeholder="Confirm Password" style={styles.input} />
+          <TextInput placeholder="Your Email Address" style={styles.input} underlineColorAndroid="transparent" />
+          <TextInput placeholder="Password" style={styles.input} underlineColorAndroid="transparent" />
+          <TextInput placeholder="Confirm Password" style={styles.input} underlineColorAndroid="transparent" />
         </View>
         <Button title="Submit" onPress={this._signInAsync} />
       </View>
@@ -38,7 +38,11 @@ const styles = StyleSheet.create({
     width: "80%"
   },
   input: {
-    width: "100%"
+    width: "100%",
+    borderWidth: 1,
+    borderColor: "#eee",
+    padding: 5,
+    margin: 8
   }
 });
 
