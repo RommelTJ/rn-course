549403cb86855b3864f49f33a7d2fb85fc478d4c
Rommel Rico <me@rommelrico.com>
2019-07-18 07:05:13 -0700

    feat(ButtonWithBackground): Initial implementation

---
+++ src/components/UI/button/ButtonWithBackground.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { TouchableHighlight, Text, View, StyleSheet } from 'react-native';
+
+const buttonWithBackground = (props) => {
+  return (
+    <TouchableHighlight onPress={props.onPress}>
+      <View style={[styles.button, {backgroundColor: props.color}]}>
+        <Text>{props.children}</Text>
+      </View>
+    </TouchableHighlight>
+  );
+};
+
+const styles = StyleSheet.create({
+  button: {
+    padding: 10,
+    margin: 5
+  }
+});
+
+export default buttonWithBackground;
