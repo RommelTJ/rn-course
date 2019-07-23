676cc946f64b35796c9ae8f422e86c0370646328
Rommel Rico <me@rommelrico.com>
2019-07-23 07:05:36 -0700

    fix(findPlace): Better icon for iOS

--- src/screens/findplace/index.js
+++ src/screens/findplace/index.js
@@ -26,7 +26,7 @@ const FindPlaceTabs = createBottomTabNavigator({
           // You can check the implementation below.
           IconComponent = FindPlaceIconWithBadge;
         } else if (routeName === 'SharePlace') {
-          iconName = Platform.OS === 'android' ? "md-share-alt" : "ios-share-alt";
+          iconName = Platform.OS === 'android' ? "md-share-alt" : "ios-share";
         }
 
         // You can return any component that you like here!
