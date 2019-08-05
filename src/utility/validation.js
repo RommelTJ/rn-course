3270d6288dfad8f347a333817bfc356e76246375
Rommel Rico <me@rommelrico.com>
2019-08-04 21:20:11 -0700

    feat(validation): Initial version

---
+++ src/utility/validation.js
@@ -0,0 +1,19 @@
+const validate = (value, rules) => {
+  let isValid = true;
+  for (let rule in rules) {
+    switch (rule) {
+      case 'isEmail':
+        isValid = isValid && emailValidator(value);
+        break;
+      case 'minLength':
+      case 'equalTo':
+      default:
+        isValid = false;
+        break;
+    }
+  }
+};
+
+const emailValidator = (value) => {
+  // TODO: Implement email validator.
+};
