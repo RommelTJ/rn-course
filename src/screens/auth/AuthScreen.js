4920d953b6ee43ba938ecda91e5f8fa708083cf9
Rommel Rico <me@rommelrico.com>
2019-08-08 07:05:47 -0700

    :bug: AuthScreen: false should be none

--- src/screens/auth/AuthScreen.js
+++ src/screens/auth/AuthScreen.js
@@ -157,7 +157,7 @@ class AuthScreen extends Component {
               onChangeText={(value) => this.updateInputState('email', value)}
               valid={this.state.controls.email.valid}
               touched={this.state.controls.email.touched}
-              autoCapitalize={false}
+              autoCapitalize={"none"}
               autoCorrect={false}
               keyboardType={"email-address"}
             />
